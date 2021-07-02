6c7dd1bafe14019a527f4e8cdbb20be2816d560c
Sascha Homeier <shomeier@apollon.de>
2021-07-02 12:12:20 +0700

    Java is pass-by-value

---
+++ java-playground-general/src/main/java/org/sho/passbyvalue/PassByValue.java
@@ -0,0 +1,61 @@
+package org.sho.passbyvalue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class Dog {
+
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Dog setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
+
+/**
+ * In Java all method arguments are always passed BY VALUE
+ */
+public class PassByValue {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PassByValue.class);
+
+    public static void main(String[] args) {
+        Dog aDog = new Dog("Max");
+        Dog oldDog = aDog;
+
+        // we pass the object to foo
+        tryReassign(aDog);
+        // aDog variable is still pointing to the "Max" dog when foo(...) returns
+        LOGGER.info("Dog's name: {}", aDog.getName());
+        LOGGER.info("aDog == oldDog: {}", aDog == oldDog);
+
+        // we pass the object to foo
+        setName(aDog);
+        // aDog variable is still pointing to the "Max" dog when foo(...) returns
+        LOGGER.info("Dog's name: {}", aDog.getName());
+        LOGGER.info("aDog == oldDog: {}", aDog == oldDog);
+    }
+
+    public static void tryReassign(Dog d) {
+        LOGGER.info("Inside scope of foo - Dog's name: {}", d.getName());
+        // change d inside of foo() to point to a new Dog instance "Fifi"
+        d = new Dog("Fifi");
+        LOGGER.info("Inside scope of foo - Dog's name: {}", d.getName());
+    }
+
+    public static void setName(Dog d) {
+        LOGGER.info("Inside scope of foo - Dog's name: {}", d.getName());
+        // change d inside of foo() to point to a new Dog instance "Fifi"
+        d.setName("Fifi");
+        LOGGER.info("Inside scope of foo - Dog's name: {}", d.getName());
+    }
+}
