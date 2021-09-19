918f96dc0bf9618862e8096265e419e6f6b3774c
Sascha Homeier <shomeier@apollon.de>
2021-09-19 19:02:07 +0700

    Added equals example

---
+++ java-playground-general/src/main/java/org/sho/equals/Equals.java
@@ -0,0 +1,13 @@
+package org.sho.equals;
+
+public class Equals {
+
+    public static void main(String[] args) {
+
+        String s1 = new String("Test");
+        String s2 = new String("Test");
+
+        System.out.println(s1 == s2); // false
+        System.out.println(s1.equals(s2)); // true
+    }
+}
