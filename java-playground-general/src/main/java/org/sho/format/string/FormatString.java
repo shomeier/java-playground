abe2d8a6d5765a7cdf7a6da7e30d1707ef965a52
Sascha Homeier <shomeier@apollon.de>
2021-10-09 11:37:49 +0700

    Added more string formatting examples

--- java-playground-general/src/main/java/org/sho/format/string/FormatString.java
+++ java-playground-general/src/main/java/org/sho/format/string/FormatString.java
@@ -6,5 +6,22 @@ public class FormatString {
 
         String format = String.format("%d %<d %2$d", 100, 200, 300);
         System.out.println(format);
+
+        // conversion to hexadecimal (base 16)
+        System.out.println("\nConversion to hexadecimal:"); // prints: 20
+        System.out.println(String.format("%x", 32)); // prints: 20
+        // using the pound flag '#'
+        System.out.println(String.format("%#x", 32)); // prints: 0x20
+        System.out.println(String.format("%#X", 32)); // prints: 0X20
+
+        // https://dzone.com/articles/java-string-format-examples
+
+        // specify a width to right-justify
+        System.out.println(String.format("|%20d|", 93)); // prints: | 93|
+        // specify a width to left-justify
+        System.out.println(String.format("|%-20d|", 93)); // prints: | 93|
+
+        System.out.println(String.format("|%30s|", "Hello World")); // prints: | Hello World|
+        System.out.println(String.format("|%-30s|", "Hello World")); // prints: |Hello World |
     }
 }
