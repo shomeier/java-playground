c7189677686f2ad0af3f23bcb6cfc72a97fac619
Sascha Homeier <shomeier@apollon.de>
2022-06-20 21:53:05 +0700

    BufferedReader Sample

---
+++ java-playground-general/src/main/java/org/sho/read/BufferedReaderDemo.java
@@ -0,0 +1,29 @@
+package org.sho.read;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class BufferedReaderDemo {
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+
+        URL resource = BufferedReaderDemo.class.getResource("/org/sho/read/utf-8.txt");
+        Path path = Paths.get(resource.toURI());
+
+        try(BufferedReader br = Files.newBufferedReader(path, StandardCharsets.ISO_8859_1)) {
+            br.lines().forEach(System.out::println);
+        }
+
+        // UTF-8 also is default
+        try(BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+            br.lines().forEach(System.out::println);
+        }
+    }
+}
