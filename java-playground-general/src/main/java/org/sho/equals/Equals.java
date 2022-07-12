dc91daadd2ff189d409cd0adc875455af8587677
Sascha Homeier <shomeier@apollon.de>
2022-07-12 12:25:41 +0700

    Added example for equals with null param

--- java-playground-general/src/main/java/org/sho/equals/Equals.java
+++ java-playground-general/src/main/java/org/sho/equals/Equals.java
@@ -7,7 +7,9 @@ public class Equals {
         String s1 = new String("Test");
         String s2 = new String("Test");
 
-        System.out.println(s1 == s2); // false
-        System.out.println(s1.equals(s2)); // true
+        System.out.println("s1 == s2: " + s1 == s2); // false
+        System.out.println("s1.equals(s2): " + s1.equals(s2)); // true
+
+        System.out.println("s1.equals(null): " + s1.equals(null));
     }
 }
